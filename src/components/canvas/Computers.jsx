64ae7a395be55d07bc43478716038b01b4eeab2c
shrutisoni08 <sonishruti008@gmail.com>
2025-02-03 16:10:40 +0530

    save changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,40 +4,38 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-// eslint-disable-next-line react/prop-types
 const Computers = ({ isMobile }) => {
-  // Load the GLTF model
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  try {
+    const computer = useGLTF("/desktop_pc/scene.gltf");
 
-  return (
-    <mesh>
-      {/* Hemisphere Light */}
-      <hemisphereLight intensity={0.15} groundColor="black"/>
+    return (
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={0.8}
+          castShadow
+          shadow-mapSize-width={512}
+          shadow-mapSize-height={512}
+        />
+        <pointLight intensity={15} />
 
-      {/* Spotlight */}
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={.8}
-        castShadow
-        shadow-mapSize-width={512}
-        shadow-mapSize-height={512}
-      />
-
-      {/* Point Light */}
-      <pointLight intensity={50} />
-
-      {/* Render the 3D model */}
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.5 : 0.80}
-        position={isMobile ? [0, -3, -2.1] : [0, -3.65, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  );
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.5 : 0.8}
+          position={isMobile ? [0, -3, -2.1] : [0, -3.65, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+    );
+  } catch (error) {
+    console.error("Failed to load model:", error);
+    return null; // Prevents crashing the app
+  }
 };
+
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
